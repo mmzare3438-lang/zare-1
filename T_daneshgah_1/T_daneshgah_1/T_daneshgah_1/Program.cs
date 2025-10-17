76799a050910c630cdd015c060c334a17643319b
mmzare3438-lang <mmzare3438@gmail.com>
2025-10-17 23:08:22 +0330

    Add files via upload

---
+++ T_daneshgah_1/T_daneshgah_1/T_daneshgah_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mohammadmahdi_Zare_T1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double a, mohit, masahat;
+
+            Console.Write("Enter a number: ");
+            a = Int32.Parse(Console.ReadLine());
+
+            mohit = 3.14 * a;
+            masahat = a * 0.5 * a * 0.5 * 3.14;
+
+            Console.WriteLine("Masahat is {0} and Mohit is {1}", masahat, mohit);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}

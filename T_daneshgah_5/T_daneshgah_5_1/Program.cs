1d86195ab5b82e9c4d991de31e64f89578c5131d
mmzare3438-lang <mmzare3438@gmail.com>
2025-11-14 17:59:21 +0330

    Add files via upload

---
+++ T_daneshgah_5/T_daneshgah_5_1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_daneshgah_5_1
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("!Triangle Calculator!");
+            Console.WriteLine("===================");
+
+            Console.Write("Enter side A: ");
+            double sideA = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter side B: ");
+            double sideB = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter side C: ");
+            double sideC = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter height for side A: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            double perimeter = CalculatePerimeter(sideA, sideB, sideC);
+            double area = CalculateArea(sideA, height);
+
+            Console.WriteLine("");
+            Console.WriteLine("Results:");
+            Console.WriteLine("Perimeter: " + perimeter);
+            Console.WriteLine("Area: " + area);
+
+            Console.ReadKey();
+        }
+
+        static double CalculatePerimeter(double a, double b, double c)
+        {
+            return a + b + c;
+        }
+
+        static double CalculateArea(double baseSide, double height)
+        {
+            return (baseSide * height) / 2;
+        }
+    }
+}
---
+++ T_daneshgah_5/T_daneshgah_5_2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T_daneshgah_5_2
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("!Multiplication of numbers!");
+            Console.WriteLine("==============================");
+
+            Console.Write("Enter first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int result = MultiplyRecursive(num1, num2);
+
+            Console.WriteLine("");
+            Console.WriteLine("Result: " + num1 + " * " + num2 + " = " + result);
+
+            Console.ReadKey();
+        }
+
+        static int MultiplyRecursive(int a, int b)
+        {
+            if (a == 0 || b == 0)
+                return 0;
+
+            if (b == 1)
+                return a;
+
+            if (b == -1)
+                return -a;
+
+            if (b > 0)
+                return a + MultiplyRecursive(a, b - 1);
+
+            else
+                return -a + MultiplyRecursive(a, b + 1);
+        }
+    }
+}
